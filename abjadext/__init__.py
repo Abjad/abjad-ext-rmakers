9c1c04636d25bfdd81dbb8766270d1f951814183
Trevor Bača <trevor.baca@gmail.com>
2019-09-29 10:53:33 -0400

    Blacked code.

--- abjadext/__init__.py
+++ abjadext/__init__.py
@@ -1,1 +1,3 @@
-__path__ = __import__("pkgutil").extend_path(__path__, __name__) # type: ignore
+__path__ = __import__("pkgutil").extend_path(  # type: ignore
+    __path__, __name__  # type: ignore
+)  # type: ignore
