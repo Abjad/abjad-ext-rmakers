746a9acae4f25b05443c48bee90854e1ce4a29af
Trevor Bača <trevor.baca@gmail.com>
2021-10-16 11:07:49 +0200

    Cleaned up LilyPond file includes.

--- abjadext/rmakers/helpers.py
+++ abjadext/rmakers/helpers.py
@@ -4,15 +4,16 @@ Helpers.
 import abjad
 
 
-def example(selection, time_signatures=None, includes=None):
+def example(selection, time_signatures=None, *, includes=None):
     """
     Makes example LilyPond file.
     """
     lilypond_file = abjad.illustrators.selection(
         selection,
         time_signatures,
-        includes=includes,
     )
+    includes = [r'\include "{_}"' for _ in includes or []]
+    lilypond_file.items[0:0] = includes
     staff = lilypond_file["Staff"]
     staff.lilypond_type = "RhythmicStaff"
     abjad.override(staff).Clef.stencil = False
